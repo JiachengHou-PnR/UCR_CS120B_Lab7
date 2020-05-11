b60828afcc9c95055e0c8a5e9fce0d20b58cf4ed
Jiacheng Hou <hou.jiacheng.w@gmail.com>
2020-05-10 20:34:06 -0700

    Initializing repository

---
+++ source/main.c
@@ -0,0 +1,22 @@
+/*	Author: houjiacheng
+ *	Lab Section: 022
+ *	Assignment: Lab 7  Exercise #
+ *	Exercise Description:
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+
+    /* Insert your solution below */
+    while (1) {
+
+    }
+    return 1;
+}
